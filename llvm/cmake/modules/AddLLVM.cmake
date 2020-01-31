ce61722b6400c1299633311b888f1e77d24a7d15
Ovidiu Panait <ovidiu.panait@windriver.com>
2020-01-31 10:56:11 +0200

    cmake: Fix configure for packages using find_package()
    Currently, when a package (i.e. bcc [https://github.com/iovisor/bcc.git])
    that depends on LLVM/Clang tries to run cmake find_package() during
    do_configure, it will fail with a similar error:
    
    |   The imported target "llvm-tblgen" references the file
    |      ".../recipe-sysroot/usr/bin/llvm-tblgen"
    |
    |   but this file does not exist.  Possible reasons include:
    |   * The file was deleted, renamed, or moved to another location.
    |   * An install or uninstall procedure did not complete successfully.
    |   * The installation package was faulty and contained
    |      ".../recipe-sysroot/usr/lib/cmake/LLVMExports.cmake"
    |   but not all the files it references.
    
    This is due to the fact that currently the cmake scripts look for target
    binaries in sysroot. Work around this by not exporting the target binaries in
    Exports-* cmake files.
    
    Upstream-Status: Inappropriate [oe-specific]
    
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -1495,7 +1495,6 @@ macro(llvm_add_tool project name)
       if( LLVM_BUILD_TOOLS )
         get_target_export_arg(${name} ${project} export_to_llvmexports)
         install(TARGETS ${name}
-                ${export_to_llvmexports}
                 RUNTIME DESTINATION ${${project}_TOOLS_INSTALL_DIR}
                 COMPONENT ${name})
         if (LLVM_ENABLE_PDB)
@@ -1511,10 +1510,8 @@ macro(llvm_add_tool project name)
         endif()
       endif()
     endif()
-    if( LLVM_BUILD_TOOLS )
-      string(TOUPPER "${project}" project_upper)
-      set_property(GLOBAL APPEND PROPERTY ${project_upper}_EXPORTS ${name})
-    endif()
+    string(TOUPPER "${project}" project_upper)
+    set_target_properties(${name} PROPERTIES FOLDER "Tools")
   endif()
   get_subproject_title(subproject_title)
   set_target_properties(${name} PROPERTIES FOLDER "${subproject_title}/Tools")
@@ -1569,7 +1566,6 @@ macro(add_llvm_utility name)
     if (LLVM_INSTALL_UTILS AND LLVM_BUILD_UTILS)
       get_target_export_arg(${name} LLVM export_to_llvmexports)
       install(TARGETS ${name}
-              ${export_to_llvmexports}
               RUNTIME DESTINATION ${LLVM_UTILS_INSTALL_DIR}
               COMPONENT ${name})
       if (LLVM_ENABLE_PDB)
@@ -1583,7 +1579,6 @@ macro(add_llvm_utility name)
                                  DEPENDS ${name}
                                  COMPONENT ${name})
       endif()
-      set_property(GLOBAL APPEND PROPERTY LLVM_EXPORTS ${name})
     elseif(LLVM_BUILD_UTILS)
       set_property(GLOBAL APPEND PROPERTY LLVM_EXPORTS_BUILDTREE_ONLY ${name})
     endif()
