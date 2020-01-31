8e8d12d7546981eef0125ff65f248b9f7c2ac4e3
Ovidiu Panait <ovidiu.panait@windriver.com>
2020-01-31 10:56:11 +0200

    cmake: Fix configure for packages using find_package()
    Currently, when a package (i.e. bcc [https://github.com/iovisor/bcc.git])
    that depends on LLVM/Clang tries to run cmake find_package() during
    do_configure, it will fail with a similar error:
    
    |   The imported target "llvm-tblgen" references the file
    |      ".../recipe-sysroot/usr/bin/llvm-tblgen"
    |
    |   but this file does not exist.  Possible reasons include:
    |   * The file was deleted, renamed, or moved to another location.
    |   * An install or uninstall procedure did not complete successfully.
    |   * The installation package was faulty and contained
    |      ".../recipe-sysroot/usr/lib/cmake/LLVMExports.cmake"
    |   but not all the files it references.
    
    This is due to the fact that currently the cmake scripts look for target
    binaries in sysroot. Work around this by not exporting the target binaries in
    Exports-* cmake files.
    
    Upstream-Status: Inappropriate [oe-specific]
    
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

--- clang/cmake/modules/AddClang.cmake
+++ clang/cmake/modules/AddClang.cmake
@@ -182,7 +182,6 @@ macro(add_clang_tool name)
     if (CLANG_BUILD_TOOLS)
       get_target_export_arg(${name} Clang export_to_clangtargets)
       install(TARGETS ${name}
-        ${export_to_clangtargets}
         RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
         COMPONENT ${name})
 
@@ -195,7 +194,6 @@ macro(add_clang_tool name)
                                  DEPENDS ${name}
                                  COMPONENT ${name})
       endif()
-      set_property(GLOBAL APPEND PROPERTY CLANG_EXPORTS ${name})
     endif()
   endif()
   set_target_properties(${name} PROPERTIES XCODE_GENERATE_SCHEME ON)
