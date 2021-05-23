11c2203f76d236ce55d2eabf6d6832f63cd43149
Maksim Kita <maksim-kita@yandex-team.ru>
2021-05-23 10:27:29 +0000

    libunwind: Added unw_backtrace method
    Source: https://github.com/ClickHouse-Extras/libunwind/commit/52f0f7861926cbfaef7e6c97d8a6d7ba2a1f6747#diff-a82fc885e2e4facf4b92d26171c13aa4aa5db296f77e1158ba2f8664e3bd1f5c
    Upstream-Status: Pending
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- libunwind/src/libunwind.cpp
+++ libunwind/src/libunwind.cpp
@@ -431,6 +431,26 @@ int __unw_remove_find_dynamic_unwind_sections(
 
 #endif // __APPLE__
 
+int unw_backtrace(void **buffer, int size) {
+  unw_context_t context;
+  unw_cursor_t cursor;
+  if (unw_getcontext(&context) || unw_init_local(&cursor, &context)) {
+    return 0;
+  }
+
+  unw_word_t ip;
+  int current = 0;
+  while (unw_step(&cursor) > 0) {
+    if (current >= size || unw_get_reg(&cursor, UNW_REG_IP, &ip)) {
+      break;
+    }
+
+    buffer[current++] = reinterpret_cast<void *>(static_cast<uintptr_t>(ip));
+  }
+
+  return current;
+}
+
 // Add logging hooks in Debug builds only
 #ifndef NDEBUG
 #include <stdlib.h>
