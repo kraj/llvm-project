e96cb3a698c66468000d29638cc2837827c02bfb
Changqing Li <changqing.li@windriver.com>
2021-12-07 04:55:48 +0000

    Correct library search path for OpenEmbedded Host
    For OpenEmbedded Host, the gcc install path is
    /usr/lib/x86_64-[distroname]-linux/[gcc-version].
    So the library search path is not found with default triple
    'x86_64-linux-gnu' for x86_64. Causing following error:
    [snip]
    compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/bin/clang
    -target x86_64-linux
    -isystem/path/to/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/include
    -O2 -pipe
    /path/to/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/share/cmake-3.21/Modules/CMakeCCompilerABI.c`
    |     /build/tmp-glibc/hosttools/ld: cannot find -lgcc
    |     /build/tmp-glibc/hosttools/ld: cannot find -lgcc
    |     clang-13: error: linker command failed with exit code 1 (use -v to see invocation)
    [snip]
    
    before this patch:
    b59da142f2b0:$ /path/to/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/bin/clang --print-search-dirs
    programs: =/build/tmp-glibc/work/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/bin
    libraries: =/build/tmp-glibc/work/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/lib/clang/13.0.1:/build/tmp-glibc/work/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/bin/../lib://lib://usr/lib
    
    after this patch:
    b59da142f2b0:$ /path/to/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/bin/clang --print-search-dirs
    programs: =/build/tmp-glibc/work/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/bin
    libraries: =/build/tmp-glibc/work/x84_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/lib/clang/13.0.1:/usr/lib/x86_64-wrs-linux/10.2.0://lib/x86_64-wrs-linux://usr/lib/x86_64-wrs-linux:/build/tmp-glibc/work/x86_64-linux/compiler-rt-native/13.0.1-r0/recipe-sysroot-native/usr/bin/../lib://lib://usr/lib
    
    Upstream-Status: Inappropriate [oe specific]
    
    Signed-off-by: Changqing Li <changqing.li@windriver.com>
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- clang/include/clang/Driver/Distro.h
+++ clang/include/clang/Driver/Distro.h
@@ -45,6 +45,7 @@ public:
     RHEL7,
     Fedora,
     Gentoo,
+    //CLANG_EXTRA_OE_DISTRO_NAME
     OpenSUSE,
     UbuntuHardy,
     UbuntuIntrepid,
@@ -134,6 +135,7 @@ public:
 
   bool IsGentoo() const { return DistroVal == Gentoo; }
 
+  //CLANG_EXTRA_OE_DISTRO_CHECK
   /// @}
 };
 
