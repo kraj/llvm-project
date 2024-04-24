70045e8cd90e4eb866e93942b2716269c4378227
Vitaly Buka <vitalybuka@google.com>
2024-04-24 09:53:55 -0700

    remove CONSTEXPRs
    Created using spr 1.3.4

--- libcxx/include/__utility/no_destroy.h
+++ libcxx/include/__utility/no_destroy.h
@@ -29,22 +29,20 @@ struct __uninitialized_tag {};
 // initialization using __emplace.
 template <class _Tp>
 struct __no_destroy {
-  _LIBCPP_CONSTEXPR_SINCE_CXX14 _LIBCPP_HIDE_FROM_ABI explicit __no_destroy(__uninitialized_tag) {}
+  _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR explicit __no_destroy(__uninitialized_tag) {}
 
   template <class... _Args>
-  _LIBCPP_CONSTEXPR _LIBCPP_HIDE_FROM_ABI explicit __no_destroy(_Args&&... __args) {
+  _LIBCPP_HIDE_FROM_ABI explicit __no_destroy(_Args&&... __args) {
     ::new ((void*)__obj_) _Tp(std::forward<_Args>(__args)...);
   }
 
   template <class... _Args>
-  _LIBCPP_CONSTEXPR_SINCE_CXX14 _LIBCPP_HIDE_FROM_ABI _Tp& __emplace(_Args&&... __args) {
+  _LIBCPP_HIDE_FROM_ABI _Tp& __emplace(_Args&&... __args) {
     return *(::new ((void*)__obj_) _Tp(std::forward<_Args>(__args)...));
   }
 
-  _LIBCPP_CONSTEXPR_SINCE_CXX14 _LIBCPP_HIDE_FROM_ABI _Tp& __get() { return *reinterpret_cast<_Tp*>(__obj_); }
-  _LIBCPP_CONSTEXPR_SINCE_CXX14 _LIBCPP_HIDE_FROM_ABI _Tp const& __get() const {
-    return *reinterpret_cast<const _Tp*>(__obj_);
-  }
+  _LIBCPP_HIDE_FROM_ABI _Tp& __get() { return *reinterpret_cast<_Tp*>(__obj_); }
+  _LIBCPP_HIDE_FROM_ABI _Tp const& __get() const { return *reinterpret_cast<const _Tp*>(__obj_); }
 
 private:
   _ALIGNAS_TYPE(_Tp) unsigned char __obj_[sizeof(_Tp)] = {};
