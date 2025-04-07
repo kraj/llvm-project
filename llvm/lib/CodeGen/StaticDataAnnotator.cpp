56a8840bc67841329b7fe2598c29dfabc097c84d
Matheus Izvekov <mizvekov@gmail.com>
2025-04-06 23:42:27 -0300

    [clang] NFC: clean trailing whitespaces in clang/test/CXX/drs/cwg15xx.cpp

--- llvm/lib/CodeGen/StaticDataAnnotator.cpp
+++ llvm/lib/CodeGen/StaticDataAnnotator.cpp
@@ -37,9 +37,100 @@
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include <set>
+
 #define DEBUG_TYPE "static-data-annotator"
 
 using namespace llvm;
+
+namespace {
+
+struct RefGraphNode;
+
+struct RefGraphEdge {
+  RefGraphEdge(RefGraphNode *Src, RefGraphNode *Dst) : Src(Src), Dst(Dst) {}
+
+  RefGraphNode *Src = nullptr;
+  RefGraphNode *Dst = nullptr;
+
+  operator RefGraphNode *() const { return Dst; }
+};
+
+// using GVWrapper = std::pair<GlobalVariable *, int>;
+
+struct RefGraphNode {
+  RefGraphNode(const GlobalVariable *Var, int ID) : Var(Var), VarID(ID) {}
+
+  struct EdgeComparator {
+    bool operator()(const RefGraphEdge &L, const RefGraphEdge &R) const {
+      return L.Dst->VarID < R.Dst->VarID;
+    }
+  };
+
+  using Edge = RefGraphEdge;
+  using RefEdges = std::set<RefGraphEdge, EdgeComparator>;
+  using iterator = RefEdges::iterator;
+  using const_iterator = RefEdges::const_iterator;
+
+  const GlobalVariable *Var;
+
+  int VarID;
+
+  RefEdges Edges;
+};
+
+// TODO: Plot dot graph.
+class RefGraph {
+public:
+  RefGraph(const Module &M) : M(M), Root(nullptr, -1) {}
+
+private:
+  void addEdge(const GlobalVariable *Src, const GlobalVariable *Dst) {
+    auto SrcNode = getOrCreateNode(Src);
+    auto DstNode = getOrCreateNode(Dst);
+    SrcNode->Edges.insert(RefGraphEdge(SrcNode, DstNode));
+  }
+
+  RefGraphNode *getOrCreateNode(const GlobalVariable *GV) {
+    auto &Node = GVToNode[GV];
+    if (Node == nullptr)
+      Node = std::make_unique<RefGraphNode>(GV, GVToNode.size());
+    return Node.get();
+  }
+
+  void computeGVDependencies(const Value *V,
+                             SmallPtrSetImpl<const GlobalVariable *> &Deps) {
+    if (auto *GV = dyn_cast<const GlobalVariable>(V)) {
+      Deps.insert(GV);
+    } else if (auto *CE = dyn_cast<const Constant>(V)) {
+      auto [Where, Inserted] = ConstantDepsCache.try_emplace(CE);
+      auto &LocalDeps = Where->second;
+      if (Inserted)
+        for (const User *CEUser : CE->users())
+          computeGVDependencies(CEUser, LocalDeps);
+      Deps.insert_range(LocalDeps);
+    }
+  }
+
+  void updateGVDependencies(const GlobalVariable *GV) {
+    SmallPtrSet<const GlobalVariable *, 4> Deps;
+    for (auto *User : GV->users())
+      computeGVDependencies(User, Deps);
+    Deps.erase(GV);
+    for (const GlobalVariable *Dep : Deps)
+      addEdge(GV, Dep);
+  }
+
+  DenseMap<const GlobalVariable *, std::unique_ptr<RefGraphNode>> GVToNode;
+
+  const Module &M;
+  RefGraphNode Root; // A dummy root
+
+  std::unordered_map<const Constant *, SmallPtrSet<const GlobalVariable *, 4>>
+      ConstantDepsCache;
+};
+
+} // namespace
 
 /// A module pass which iterates global variables in the module and annotates
 /// their section prefixes based on profile-driven analysis.
