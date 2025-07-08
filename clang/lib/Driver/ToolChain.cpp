5c33600b712a921b9e2d46acd39a307226872fea
Khem Raj <raj.khem@gmail.com>
2025-07-07 19:22:07 -0700

    clang: Use sysroot relative paths for getArchSpecificLibPaths and getRuntimePath
    Clang's default sysroot implementation differs that what we do in OE,
    and we do it with gcc as well where we install c++ runtime headers in
    target sysroot, clang does not by default it puts them relative to its
    own installation and uses resourcedir to point to it. In order to solve
    this we have to raise this question with respective upstreams or rethink
    about how we package compiler runtime.
    
    Upstream-Status: Inappropriate [OE-Specific]
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -1193,7 +1193,10 @@ std::optional<std::string> ToolChain::getDefaultIntrinsicModuleDir() const {
 }
 
 std::optional<std::string> ToolChain::getRuntimePath() const {
-  SmallString<128> P(D.ResourceDir);
+  SmallString<128> P(D.SysRoot);
+  StringRef ClangLibdirBasename(CLANG_INSTALL_LIBDIR_BASENAME);
+  llvm::sys::path::append(P, "/usr/", ClangLibdirBasename, "clang",
+                          CLANG_VERSION_STRING);
   llvm::sys::path::append(P, "lib");
   if (auto Ret = getTargetSubDirPath(P))
     return Ret;
@@ -1221,7 +1224,10 @@ ToolChain::path_list ToolChain::getArchSpecificLibPaths() const {
   path_list Paths;
 
   auto AddPath = [&](const ArrayRef<StringRef> &SS) {
-    SmallString<128> Path(getDriver().ResourceDir);
+    SmallString<128> Path(getDriver().SysRoot);
+    StringRef ClangLibdirBasename(CLANG_INSTALL_LIBDIR_BASENAME);
+    llvm::sys::path::append(Path, "/usr/", ClangLibdirBasename, "clang",
+                            CLANG_VERSION_STRING);
     llvm::sys::path::append(Path, "lib");
     for (auto &S : SS)
       llvm::sys::path::append(Path, S);
