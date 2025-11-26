ec3c5626ff4ae6a9481cc7f771f72a784fa59336
Akash Banerjee <Akash.Banerjee@amd.com>
2025-11-26 16:54:20 +0000

    Update tests.

--- offload/test/offloading/fortran/target-is-device-ptr.f90
+++ offload/test/offloading/fortran/target-is-device-ptr.f90
@@ -1,79 +1,46 @@
-! Validate that a device pointer allocated via OpenMP runtime APIs can be
-! consumed by a TARGET region using the is_device_ptr clause.
+! Validate that a device pointer obtained via omp_get_mapped_ptr can be used
+! inside a TARGET region with the is_device_ptr clause.
 ! REQUIRES: flang, amdgcn-amd-amdhsa
-! UNSUPPORTED: nvptx64-nvidia-cuda
-! UNSUPPORTED: nvptx64-nvidia-cuda-LTO
-! UNSUPPORTED: aarch64-unknown-linux-gnu
-! UNSUPPORTED: aarch64-unknown-linux-gnu-LTO
-! UNSUPPORTED: x86_64-unknown-linux-gnu
-! UNSUPPORTED: x86_64-unknown-linux-gnu-LTO
 
 ! RUN: %libomptarget-compile-fortran-run-and-check-generic
 
 program is_device_ptr_target
-  use omp_lib
-  use iso_c_binding
+  use iso_c_binding, only : c_ptr, c_loc
   implicit none
 
+  interface
+    function omp_get_mapped_ptr(host_ptr, device_num)                       &
+        bind(C, name="omp_get_mapped_ptr")
+      use iso_c_binding, only : c_ptr, c_int
+      type(c_ptr) :: omp_get_mapped_ptr
+      type(c_ptr), value :: host_ptr
+      integer(c_int), value :: device_num
+    end function omp_get_mapped_ptr
+  end interface
+
   integer, parameter :: n = 4
-  integer, target :: host(n)
-  type(c_ptr) :: device_ptr
-  integer(c_int) :: rc
-  integer :: i
+  integer, parameter :: dev = 0
+  integer, target :: a(n)
+  type(c_ptr) :: dptr
+  integer :: flag
 
-  do i = 1, n
-    host(i) = i
-  end do
+  a = [2, 4, 6, 8]
+  flag = 0
 
-  device_ptr = omp_target_alloc(int(n, c_size_t) * int(c_sizeof(host(1)), c_size_t), &
-                                omp_get_default_device())
-  if (.not. c_associated(device_ptr)) then
-    print *, "device alloc failed"
-    stop 1
-  end if
+  !$omp target data map(tofrom: a, flag)
+    dptr = omp_get_mapped_ptr(c_loc(a), dev)
 
-  rc = omp_target_memcpy(device_ptr, c_loc(host), &
-                         int(n, c_size_t) * int(c_sizeof(host(1)), c_size_t), &
-                         0_c_size_t, 0_c_size_t, &
-                         omp_get_default_device(), omp_get_initial_device())
-  if (rc .ne. 0) then
-    print *, "host->device memcpy failed"
-    call omp_target_free(device_ptr, omp_get_default_device())
-    stop 1
-  end if
+    !$omp target is_device_ptr(dptr) map(tofrom: flag)
+      flag = flag + 1
+    !$omp end target
+  !$omp end target data
 
-  call fill_on_device(device_ptr)
-
-  rc = omp_target_memcpy(c_loc(host), device_ptr, &
-                         int(n, c_size_t) * int(c_sizeof(host(1)), c_size_t), &
-                         0_c_size_t, 0_c_size_t, &
-                         omp_get_initial_device(), omp_get_default_device())
-  call omp_target_free(device_ptr, omp_get_default_device())
-
-  if (rc .ne. 0) then
-    print *, "device->host memcpy failed"
-    stop 1
-  end if
-
-  if (all(host == [2, 4, 6, 8])) then
+  if (flag .eq. 1 .and. all(a == [2, 4, 6, 8])) then
     print *, "PASS"
   else
-    print *, "FAIL", host
+    print *, "FAIL", a
   end if
 
-contains
-  subroutine fill_on_device(ptr)
-    type(c_ptr) :: ptr
-    integer, pointer :: p(:)
-    call c_f_pointer(ptr, p, [n])
-
-    !$omp target is_device_ptr(ptr)
-      p(1) = 2
-      p(2) = 4
-      p(3) = 6
-      p(4) = 8
-    !$omp end target
-  end subroutine fill_on_device
 end program is_device_ptr_target
 
 !CHECK: PASS
