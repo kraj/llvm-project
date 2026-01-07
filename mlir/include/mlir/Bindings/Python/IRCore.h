256fff028969ba360b89edb98c1aea147fd9cea6
Rolf Morel <rolf.morel@intel.com>
2026-01-07 12:05:58 -0800

    [MLIR][Python] Expose TransformOpInterface and MemoryEffectsOpInterfaces
    Enables implementing these two interfaces from Python through defining
    the relevant methods on a Python class which then serve as callbacks for
    a new FallbackModel C++ class that acts as a "trampoline" to Python for
    when the Interface's methods are called from C++. Like in the C++
    codebase, these FallbackModels are a late-binding mechanism which can be
    attached to an operation after its definition.

--- mlir/include/mlir/Bindings/Python/IRCore.h
+++ mlir/include/mlir/Bindings/Python/IRCore.h
@@ -1496,6 +1496,7 @@ private:
 class MLIR_PYTHON_API_EXPORTED PyOpOperand {
 public:
   PyOpOperand(MlirOpOperand opOperand) : opOperand(opOperand) {}
+  operator MlirOpOperand() const { return opOperand; }
 
   nanobind::typed<nanobind::object, PyOpView> getOwner() const;
 
@@ -1602,6 +1603,7 @@ public:
   static constexpr IsAFunctionTy isaFunction = mlirValueIsAOpResult;
   static constexpr const char *pyClassName = "OpResult";
   using PyConcreteValue::PyConcreteValue;
+  operator MlirOpResult() { return mlirValueToOpResult(castFrom(*this)); }
 
   static void bindDerived(ClassTy &c);
 };
@@ -1838,13 +1840,20 @@ private:
 MLIR_PYTHON_API_EXPORTED MlirValue getUniqueResult(MlirOperation operation);
 MLIR_PYTHON_API_EXPORTED void populateIRCore(nanobind::module_ &m);
 MLIR_PYTHON_API_EXPORTED void populateRoot(nanobind::module_ &m);
+
+/// Helper for creating an @classmethod.
+template <class Func, typename... Args>
+static nanobind::object classmethod(Func f, Args... args) {
+  nanobind::object cf = nanobind::cpp_function(f, args...);
+  return nanobind::borrow<nanobind::object>((PyClassMethod_New(cf.ptr())));
+}
+
 } // namespace MLIR_BINDINGS_PYTHON_DOMAIN
 } // namespace python
 } // namespace mlir
 
 namespace nanobind {
 namespace detail {
-
 template <>
 struct type_caster<
     mlir::python::MLIR_BINDINGS_PYTHON_DOMAIN::DefaultingPyMlirContext>
