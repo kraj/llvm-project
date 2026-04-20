e39b7bb1dc9f1926b086d889f60676c081da1643
pvanhout <pierre.vanhoutryve@amd.com>
2026-04-20 14:14:16 +0200

    [NFCI][clang] Allow overriding any global variable address space
    Allow the target to change the AS of a global variable at will, not just whenever Clang cannot assign one.
    This enables the next patch that will specialize LDS GVs for barriers as a separate address space.

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -6140,7 +6140,9 @@ CharUnits CodeGenModule::GetTargetTypeStoreSize(llvm::Type *Ty) const {
       getDataLayout().getTypeStoreSizeInBits(Ty));
 }
 
-LangAS CodeGenModule::GetGlobalVarAddressSpace(const VarDecl *D) {
+static std::optional<LangAS>
+getGlobalVarDefaultAddressSpace(const VarDecl *D, const LangOptions &LangOpts,
+                                CGOpenMPRuntime *OpenMPRuntime) {
   if (LangOpts.OpenCL) {
     LangAS AS = D ? D->getType().getAddressSpace() : LangAS::opencl_global;
     assert(AS == LangAS::opencl_global ||
@@ -6175,7 +6177,14 @@ LangAS CodeGenModule::GetGlobalVarAddressSpace(const VarDecl *D) {
     if (OpenMPRuntime->hasAllocateAttributeForGlobalVar(D, AS))
       return AS;
   }
-  return getTargetCodeGenInfo().getGlobalVarAddressSpace(*this, D);
+
+  return std::nullopt;
+}
+
+LangAS CodeGenModule::GetGlobalVarAddressSpace(const VarDecl *D) {
+  std::optional<LangAS> AS =
+      getGlobalVarDefaultAddressSpace(D, LangOpts, OpenMPRuntime.get());
+  return getTargetCodeGenInfo().adjustGlobalVarAddressSpace(*this, D, AS);
 }
 
 LangAS CodeGenModule::GetGlobalConstantAddressSpace() const {
