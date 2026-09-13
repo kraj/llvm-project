da57c792cdcd9a423681f821298a83ca17ddcc34
benedekaibas <kaibas01@allegheny.edu>
2026-09-13 21:59:50 +0200

    [analyzer][NFC] Determine object parameter constness for getExtraInvalidatedValues

--- clang/lib/StaticAnalyzer/Core/CallEvent.cpp
+++ clang/lib/StaticAnalyzer/Core/CallEvent.cpp
@@ -758,6 +758,16 @@ const FunctionDecl *CXXInstanceCall::getDecl() const {
   return getSVal(CE->getCallee()).getAsFunctionDecl();
 }
 
+static bool hasConstObjectParameter(const CXXMethodDecl *MD) {
+  if (!MD->isExplicitObjectMemberFunction())
+    return MD->isConst();
+
+  QualType ObjTy = MD->getParamDecl(0)->getType();
+  if (const auto *RT = ObjTy->getAs<ReferenceType>())
+    return RT->getPointeeType().isConstQualified();
+  return true;
+}
+
 void CXXInstanceCall::getExtraInvalidatedValues(
     ValueList &Values, RegionAndSymbolInvalidationTraits *ETraits) const {
   SVal ThisVal = getCXXThisVal();
@@ -765,7 +775,7 @@ void CXXInstanceCall::getExtraInvalidatedValues(
 
   // Don't invalidate if the method is const and there are no mutable fields.
   if (const auto *D = cast_or_null<CXXMethodDecl>(getDecl())) {
-    if (!D->isConst())
+    if (!hasConstObjectParameter(D))
       return;
 
     // Get the record decl for the class of 'This'. D->getParent() may return
