afe2b14b694302a7e2188d2a614f85009ea55306
Dmitry Sidorov <Dmitry.Sidorov@amd.com>
2026-09-23 00:19:00 +0200

    format

--- llvm/lib/Target/AMDGPU/AMDGPUTargetTransformInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetTransformInfo.cpp
@@ -1155,7 +1155,7 @@ InstructionCost GCNTTIImpl::getCastInstrCost(unsigned Opcode, Type *Dst,
       if (SrcBits > 16)
         return Scale(FPTy->isHalfTy() ? 3 : 2);
       if (FPTy->isHalfTy() && ST->has16BitInsts())
-        return Scale(Narrow ? (IsSigned ? 3 : 2)
+        return Scale(Narrow                           ? (IsSigned ? 3 : 2)
                      : SrcBits == 8 && !ST->hasSDWA() ? 2
                                                       : 1);
       const InstructionCost FloatCost =
